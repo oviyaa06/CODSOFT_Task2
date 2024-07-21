91bc9cb4c5e3d343bb01c5b217c7406808250c20
oviyaa06 <111644553+oviyaa06@users.noreply.github.com>
2024-07-21 09:56:13 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,32 @@
+# Simple Arithimetic Calculator - TASK 2
+print(""" 
+Enter any operation:- 
+    addition -> +
+    subtraction -> -
+    multiplication -> *
+    division -> /
+    remainder -> %
+    exponent -> **
+      """)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+oper = input("Enter operation to perform: ")
+if oper == '+':
+    print(f"Addition of num1 and num2 is: {num1+num2}")
+elif oper == '-':
+    print(f"Subtraction of num1 and num2 is: {num1-num2}")
+elif oper == '*':
+    print(f"Multiplication of num1 and num2 is: {num1*num2}")
+elif oper == '/':
+    print(f"Division of num1 and num2 is: {num1/num2}")
+elif oper == '%':
+    print(f"Remainder of num1 and num2 is: {num1%num2}")
+elif oper == '**':
+    print(f"Exponent of num1 and num2 is: {num1**num2}")
+else:
+    print("Invalid operator!")
+
+
+
+    
